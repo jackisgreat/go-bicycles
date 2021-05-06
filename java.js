2a78c761282ae666a3720d17e4ac90d94f95c334
jackisgreat <thejackfarrell@gmail.com>
2021-05-06 15:21:36 -0700

    Lil java it looks like

--- java.js
+++ java.js
@@ -3,29 +3,28 @@ $(document).ready(function() {
 console.log('JS file loaded');
 
     
-// drop down shop menu    
+// drop down shop menu    ---- color changing but not removing on leaving
 $('.nav-landing').hover(
   function () {
-    $('#drop-down').show();
-  }, 
+        $('#drop-down').show();
+        $(this).css('color','green')
+;})
     
-    $(document).on('mouseleave', '#drop-down', function () {
-    $('#drop-down').hide();
-})
+$(document).on('mouseleave', '#drop-down', 
+    function () {
+        $('#drop-down').hide();
+        $('nav-landing ul').css('color','#222222')
+;})
   
-  
-);
-
-});
 
 // header shrink on scroll
 
 $(function(){
-  $('#header').data('size','big');
+        $('#header').data('size','big');
 });
 
 $(window).scroll(function(){
-  if($(document).scrollTop() > 0)
+    if($(document).scrollTop() > 0)
 {
     if($('#header').data('size') == 'big')
     {
@@ -45,4 +44,5 @@ else
         },200);
       }  
   }
+});
 });
